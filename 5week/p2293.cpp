ac2d17b6bf36e46e7ae620b33c0671565fcfc9e9
root <root@DESKTOP-FHNLA9O>
2023-07-30 00:42:41 +0900

    end

---
+++ 5week/p2293.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[10001];
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int coin[n];
+
+    for(int i = 1; i <= n; i++)
+        cin >> coin[i];
+
+    dp[0] = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = coin[i]; j <= k; j++)
+            dp[j] += dp[j - coin[i]];
+    }
+
+    cout << dp[k];
+
+    return 0;
+}
