62f765ce2ede0f82fd7f3f5199a6ff88c847b32c
root <root@DESKTOP-U0H4RQ4>
2023-09-11 13:06:28 +0900

    tmp

---
+++ 7week/ExampleQueue.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m, a, b;
+queue<int> q;
+vector<int> graph[32001];
+int indgree[32001];
+
+void BFS(int size) {
+    for(int i = 1; i <= size; i++) {
+        if(indgree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()) {
+        int idx = q.front();
+        q.pop();
+
+        cout << idx << " ";
+
+        for(int i = 0; i < graph[idx].size(); i++) {
+            if(indgree[graph[idx][i]] == 1)
+                q.push(graph[idx][i]);
+            if(indgree[graph[idx][i]] > 0)
+                indgree[graph[idx][i]]--;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++) {
+        cin >> a >> b;
+        graph[a].push_back(b);
+        indgree[b]++;
+    }
+
+    BFS(n);
+    cout << endl;
+
+    return 0;
+}
